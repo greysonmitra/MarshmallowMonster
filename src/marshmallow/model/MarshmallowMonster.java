a05d10a331d4416e5abff8f51c5ceeaf04eda3ba
greysonmitra <greyson.mitra@outlook.com>
2015-09-09 19:55:44 -0600

    Monster Project 2.0
    To use as a copy and paste for continuing the project at school.

---
+++ src/marshmallow/model/MarshmallowMonster.java
@@ -0,0 +1,24 @@
+package marshmallow.model;
+
+public class MarshmallowMonster 
+{
+	private String monsterName;
+	private int monsterEyes;
+	private int monsterNoses;
+	private double monsterHair;
+	private double monsterlegs;
+	private boolean monsterBellyButton;
+	
+	private MarshmallowMonster()
+	
+	public MarshmallowMonster(String monsterName, int monsterEyes, int monsterNoses, double monsterHair, double monsterLegs, boolean monsterBellyButton )
+	
+	{
+		this.monsterName = monsterName;
+		this.monsterEyes = monsterEyes;
+		this.monsterNoses = monsterNoses;
+		this.monsterLegs = monsterLegs;
+		this.monsterHair = monsterHair;
+		this.monsterBellyButton = monsterBellyButton;
+	}
+}
---
+++ src/marshmallows/controller/MonsterController.java
@@ -0,0 +1,26 @@
+package marshmallows.controller;
+
+public class MonsterController 
+{
+	private MarshmallowMonster greysonMonster;
+	private MarshmallowOutput myOutput;
+	
+	public MonsterController()
+	{
+		int eyes = 4;
+		double legs = 8;
+		double hair = 1;
+		int noses = 1;
+		boolean hasbellybutton = false;
+		String name = "Fuddy Duddy";
+		
+		myOutput = new MarshmallowOutput();
+		greysonMonster = new MarshmallowMonster(name, eyes, noses, hair, legs, hasbellybutton); //must match constructor in MarhmallowMonster.java
+	}
+	
+	public void start()
+	{
+		myOutput.displayMonsterInfo(greysonMonster.toString());
+	}
+
+}
